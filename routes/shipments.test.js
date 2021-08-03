6459e65695c00628241b9a3f39489adae0c034f2
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-08-03 16:21:43 -0700

    finished part 1 except for that part about tests haha

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -15,4 +15,34 @@ describe("POST /", function () {
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
+  test("invalid productId", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 999,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+  test("valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+  test("valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
 });
